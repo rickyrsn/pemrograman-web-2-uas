5f153fecf953664a5ba9871b956e709c0b01dfb6
rickysut25 <ricky.sutjipto@sirclo.com>
2023-12-24 11:25:15 +0700

    change database

--- admin.php
+++ admin.php
@@ -5,26 +5,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Page</title>
     <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+        header {
+            background-color: #333;
+            padding: 10px;
+            text-align: center;
+            color: #fff;
+        }
         form {
-            width: 50%;
+            width: 80%;
             margin: 20px auto;
+            background-color: #fff;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            padding: 20px;
+            border-radius: 8px;
         }
         label, input {
             display: block;
             margin-bottom: 10px;
         }
+        input[type="text"],
+        input[type="time"],
+        input[type="submit"] {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ddd;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
         input[type="submit"] {
             background-color: #4caf50;
             color: white;
-            padding: 10px;
-            border: none;
             cursor: pointer;
+        }
+        input[type="submit"]:hover {
+            background-color: #45a049;
         }
     </style>
 </head>
 <body>
-    <h2>Admin Page</h2>
-    
+    <header>
+        <h1>Admin Page</h1>
+    </header>
     <form action="insert.php" method="post">
         <label for="mata_kuliah">Mata Kuliah:</label>
         <input type="text" name="mata_kuliah" required>
